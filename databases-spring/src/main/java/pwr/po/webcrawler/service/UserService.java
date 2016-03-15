f19d0da21d9dfbd7fce459e7a7e7d920061fc9bc
Rafal <rkpieniazek@gmail.com>
2016-03-15 21:07:27 +0100

    small cleanup

--- databases-spring/src/main/java/pwr/po/webcrawler/service/UserService.java
+++ databases-spring/src/main/java/pwr/po/webcrawler/service/UserService.java
@@ -1,20 +1,14 @@
 package pwr.po.webcrawler.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import pwr.po.webcrawler.model.User;
 import pwr.po.webcrawler.repository.UserRepository;
 
 import java.util.List;
 
-/**
- * Created by pawel on 07.12.15.
- */
 @Service
 public class UserService {
-
-    private String asyncMsg = "in progress";
 
     @Autowired
     private UserRepository userRepository;
@@ -27,19 +21,5 @@ public class UserService {
         userRepository.save(u);
     }
 
-//    @Async // a co to takie fajne?
-//    public void doAsync() {
-//        System.out.println("start async");
-//        try {
-//            Thread.sleep(10000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        asyncMsg = "done";
-//        System.out.println("finish async");
-//    }
 
-//    public String getAsyncMsg() {
-//        return asyncMsg;
-//    }
 }
