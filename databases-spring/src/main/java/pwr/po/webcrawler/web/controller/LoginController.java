25b14b16836998a305ccf73ec34177c4c615cd1c
Rafal <rkpieniazek@gmail.com>
2016-04-06 22:09:11 +0200

    code review fixes

--- databases-spring/src/main/java/pwr/po/webcrawler/web/controller/LoginController.java
+++ databases-spring/src/main/java/pwr/po/webcrawler/web/controller/LoginController.java
@@ -1,10 +1,7 @@
 package pwr.po.webcrawler.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import pwr.po.webcrawler.model.user.User;
 import pwr.po.webcrawler.service.auth.TokenAuthenticationService;
 import pwr.po.webcrawler.service.user.UserService;
 
@@ -17,10 +14,10 @@ public class LoginController {
     @Autowired
     TokenAuthenticationService tokenAuthenticationService;
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public User login(){
-        return new User("testUserName","userFirstName","lastName");
-    }
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
+//    public User login(){
+//        return
+//    }
 
 
 
