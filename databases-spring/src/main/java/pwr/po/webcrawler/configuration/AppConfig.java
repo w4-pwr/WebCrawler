f2abe7a90efcb519ddec9c0aeff104243cc38185
Rafal <rkpieniazek@gmail.com>
2016-04-06 20:31:45 +0200

    flyway config

--- databases-spring/src/main/java/pwr/po/webcrawler/configuration/AppConfig.java
+++ databases-spring/src/main/java/pwr/po/webcrawler/configuration/AppConfig.java
@@ -1,16 +1,19 @@
 package pwr.po.webcrawler.configuration;
 
 import com.googlecode.flyway.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import pwr.po.webcrawler.service.auth.TokenAuthenticationService;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @ComponentScan(basePackages = "pwr.po.webcrawler", excludeFilters = @ComponentScan.Filter(value = Configuration.class, type = FilterType.ANNOTATION))
@@ -23,22 +26,19 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         registry.addMapping("/**").exposedHeaders(TokenAuthenticationService.AUTH_HEADER_NAME);
     }
 
+    @Autowired
+    DataSource dataSource;
+
+
     @Bean(initMethod = "migrate")
     public Flyway flyway() {
         Flyway flyway = new Flyway();
+        flyway.setDataSource(dataSource);
         flyway.clean();
         flyway.init();
         //flyway.setInitOnMigrate(true);
-        //flyway.setSchemas("SBA_DIALOG");
-        //flyway.setLocations("filesystem:src/main/resources/db/migration");
-        //flyway.setDataSource("jdbc:postgresql://localhost:5432", "sa", null);
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://ec2-54-228-246-206.eu-west-1.compute.amazonaws.com:5432/dabau342amt24s?&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
-        dataSource.setUsername("jnheqfbnlmxjmy");
-        dataSource.setPassword("AERYkjjbHNvIP_9ERFd7XSrMZj");
-        flyway.setDataSource(dataSource);
-        //flyway.setLocations("info.novatec.eap.persistence.migration", "filesystem:src/main/resources/migrations");
+        flyway.setSchemas("dabau342amt24s");
+
         flyway.migrate();
         return flyway;
     }
