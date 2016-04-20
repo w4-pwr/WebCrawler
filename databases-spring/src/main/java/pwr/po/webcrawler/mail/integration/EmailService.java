875481f77aa10f7d8957a3f64d299ab135f2f5e5
holkiew <olkiewiczhubert@gmail.com>
2016-04-20 13:09:40 +0200

    Tests removed Small corrections

--- databases-spring/src/main/java/pwr/po/webcrawler/mail/integration/EmailService.java
+++ databases-spring/src/main/java/pwr/po/webcrawler/mail/integration/EmailService.java
@@ -1,7 +1,6 @@
 package pwr.po.webcrawler.mail.integration;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import pwr.po.webcrawler.configuration.ConfigService;
 import pwr.po.webcrawler.configuration.Keys;
@@ -15,7 +14,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
-@PropertySource("classpath:app.properties")
 @Service
 public class EmailService {
 
