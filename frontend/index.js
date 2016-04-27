3fbeaa79cc05bf930dd49dc74715005f15c54c91
Karol <karol.selak@gmail.com>
2016-04-27 10:49:05 +0200

    after merge with master

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
-import App from './modules/App'
+import Homepage from './modules/Homepage'
 
 render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={Homepage}/>
+    <Route path="/user" component={Homepage}/>
   </Router>
 ), document.getElementById('app'))
