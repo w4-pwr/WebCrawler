4742d623bd6e73cc95397c97970486d22e2cf7a3
Adam Zimny <adamzimnyy@gmail.com>
2016-06-01 23:42:36 +0200

    zmiana uploadu avatara z naszego serwera na imgur.com, naprawione testy do tego

---
+++ databases-spring/src/main/java/pwr/po/webcrawler/model/ImgurData.java
@@ -0,0 +1,7 @@
+package pwr.po.webcrawler.model;
+
+/**
+ * Created by Adam on 2016-06-01.
+ */
+public class ImgurData {
+}
---
+++ databases-spring/src/main/java/pwr/po/webcrawler/web/ImgurAPI.java
@@ -0,0 +1,7 @@
+package pwr.po.webcrawler.web;
+
+/**
+ * Created by Adam on 2016-06-01.
+ */
+public class ImgurAPI {
+}
